102e6b0da874c94d7a5f31fd112a9bc6f05cf4af
William Dale <wd40bug@gmail.com>
2026-07-31 17:21:31 -0600

    generation 24

--- home/wd40bug/home.nix
+++ home/wd40bug/home.nix
@@ -30,7 +30,16 @@ in
     luajit
     taplo
     jetbrains-mono
-  ] ++ lib.optionals osConfig.hostConfig.GUI [kitty];
+  ] ++ lib.optionals osConfig.hostConfig.GUI [bitwarden-desktop];
+
+  programs.kitty = {
+    enable = osCOnfig.hostConfig.GUI;
+    # Your existing settings...
+    extraConfig = ''
+      # Force Kitty's built-in Nerd Font for symbols, bypassing DejaVu
+      symbol_map U+e000-U+e00a,U+e0a0-U+e0a2,U+e0a3,U+e0b0-U+e0b3,U+e0b4-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ea60-U+ec1e,U+ed00-U+efce,U+f000-U+f2ff,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono
+    '';
+  };
 
   programs.firefox = {
     enable = osConfig.hostConfig.GUI;
