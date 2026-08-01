7466b446a87fbcb35c4195d11ac081d2d71d3fc8
William Dale <wd40bug@gmail.com>
2026-08-01 17:02:12 -0600

    generation 40

--- home/wd40bug/home.nix
+++ home/wd40bug/home.nix
@@ -129,13 +129,4 @@ in
       # "org/gnome/settings-daemon/plugings/media-keys/custom-keybindings" = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
     }; 
   };
-
-/org/gnome/desktop/interface/clock-format
-  '12h'
-
-/org/gtk/settings/file-chooser/clock-format
-  '12h'
-
-/org/gnome/desktop/interface/clock-show-weekday
-  true
 }
