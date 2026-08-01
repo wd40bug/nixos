185a350cf6bcb2eddcdce1b4c7a5c8f8df1b25e3
William Dale <wd40bug@gmail.com>
2026-08-01 13:27:50 -0600

    generation 32

--- home/wd40bug/home.nix
+++ home/wd40bug/home.nix
@@ -120,6 +120,6 @@ in
       command = "kitty";
       name = "Open Kitty";
     };
-    "org/gnome/settings-daemon/plugings/media-keys/custom-keybindings" = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
+    # "org/gnome/settings-daemon/plugings/media-keys/custom-keybindings" = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
   }; 
 }
