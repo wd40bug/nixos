010a8720121d38ffed02849a1f2f32cc045d1ce6
William Dale <wd40bug@gmail.com>
2026-08-01 13:36:22 -0600

    generation 36

--- hosts/seville/default.nix
+++ hosts/seville/default.nix
@@ -21,7 +21,7 @@
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
 
-    environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs epiphany gnome-maps ];
+    environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs epiphany gnome-maps gnome-console ];
 
     services.xserver.xkb = {
       layout = "us";
