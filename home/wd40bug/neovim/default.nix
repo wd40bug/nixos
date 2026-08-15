1369979a74f1d224431d9fbe68d4de969e3925d1
William Dale <wd40bug@gmail.com>
2026-08-15 11:16:22 -0600

    generation 90

--- home/wd40bug/neovim/default.nix
+++ home/wd40bug/neovim/default.nix
@@ -1,41 +1,44 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 {
-  home.packages = with pkgs; [
-    lua-language-server
-    tree-sitter
-    clang-tools
-    file
-    sqlite
-    jq
-    ruff
-    xmlformat
-    gdb
-    xclip
-    lua5_1
-    rustup
-    fd
-    fish-lsp
-    nil
-    gopls
-    kotlin-language-server
-    pyright
-    typescript-language-server
-    verible
-  ];
+  inputs.custom.neovim.enable = lib.mkEnableOption "Enable neovim configuration";
+  config = lib.mkIf config.custom.neovim.enable {
+    home.packages = with pkgs; [
+      lua-language-server
+      tree-sitter
+      clang-tools
+      file
+      sqlite
+      jq
+      ruff
+      xmlformat
+      gdb
+      xclip
+      lua5_1
+      rustup
+      fd
+      fish-lsp
+      nil
+      gopls
+      kotlin-language-server
+      pyright
+      typescript-language-server
+      verible
+    ];
 
-  xdg.desktopEntries.nvim = {
-    name = "Neovim";
-    noDisplay = true;
-    exec = "nvim %F";
-    icon = "nvim";
-    terminal = true;
-    type = "Application";
-  };
+    xdg.desktopEntries.nvim = {
+      name = "Neovim";
+      noDisplay = true;
+      exec = "nvim %F";
+      icon = "nvim";
+      terminal = true;
+      type = "Application";
+    };
 
-  xdg.configFile = {
-    "nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/wd40bug/neovim/nvim";
-      recursive = true;
+    xdg.configFile = {
+      "nvim" = {
+        source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/home/wd40bug/neovim/nvim";
+        recursive = true;
+      };
     };
   };
 }
