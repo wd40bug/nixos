c2dda5d790a5dab6577efc53c7a41c9e9735d784
William Dale <wd40bug@gmail.com>
2026-08-16 09:27:19 -0600

    generation 121

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -30,7 +30,7 @@
       # };
       image = pkgs.fetchurl {
         url = "https://wallup.net/wp-content/uploads/2018/09/28/687841-artistic-art-artwork-painting-original.jpg";
-        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        hash = "sha256-2oKTfLP9IaKtaDLdnWu2UIKcgMlj/Hu0BddqLnW8Eps=";
       };
     };
   };
