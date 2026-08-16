4e085e1549c26c6ff744ccc2627303eaff060cb9
William Dale <wd40bug@gmail.com>
2026-08-16 12:34:48 -0600

    generation 126

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -21,30 +21,8 @@
   config = lib.mkIf config.custom.stylix.enable {
     stylix = {
       enable = true;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
       polarity = "dark";
-      targets = {
-        fish.colors.override = {
-          withHashtag = {
-            base00 = "#ffffff";
-            base01 = "#ffffff";
-            base02 = "#ffffff";
-            base03 = "#ffffff";
-            base04 = "#ffffff";
-            base05 = "#ffffff";
-            base06 = "#ffffff";
-            base07 = "#ffffff";
-            base08 = "#ffffff";
-            base09 = "#ffffff";
-            base0A = "#ffffff";
-            base0B = "#ffffff";
-            base0C = "#ffffff";
-            base0D = "#ffffff";
-            base0E = "#ffffff";
-            base0F = "#ffffff";
-          };
-        };
-      };
       # image = pkgs.fetchurl {
       #   url = "https://wallup.net/wp-content/uploads/2018/09/28/687841-artistic-art-artwork-painting-original.jpg";
       #   hash = "sha256-2oKTfLP9IaKtaDLdnWu2UIKcgMlj/Hu0BddqLnW8Eps=";
