c7a7673a99d4fb54f92aa6d86981b67feb505399
William Dale <wd40bug@gmail.com>
2026-08-17 19:37:19 -0600

    generation 160

--- modules/wireshark.nix
+++ modules/wireshark.nix
@@ -1,10 +1,11 @@
-{config, lib, ...}:
+{pkgs, config, lib, ...}:
 {
   options.custom.wireshark.enable = lib.mkEnableOption "Custom Wireshark Module";
 
   config = lib.mkIf config.custom.wireshark.enable {
     programs.wireshark = {
       enable = true;
+      package = pkgs.wireshark;
     };
   };
 }
