115f5728413d40be71ee77b9dd7dc76497d5fba8
William Dale <wd40bug@gmail.com>
2026-08-17 19:46:53 -0600

    generation 162

--- hosts/seville/configuration.nix
+++ hosts/seville/configuration.nix
@@ -9,7 +9,7 @@
     ./../../modules/gnome.nix
     ./../../modules/xserver.nix
     ./../../modules/core
-    ./../../modules/wireshark.nix
+    ./../../modules/guiapps
   ];
 
   config = {
@@ -28,7 +28,10 @@
       xserver = {
         enable = true;
       };
-      wireshark.enable = true;
+      guiapps = {
+        enable = true;
+        wireshark.enable = true;
+      };
     };
 
     users.users.wd40bug = {
