f4785b541aa5b7f191bbf3d71219eafeddbf757f
William Dale <wd40bug@gmail.com>
2026-08-24 23:54:02 +0000

    generation 186

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -69,6 +69,7 @@
 
   # Set your time zone.
   services.automatic-timezoned.enable = true;
+  services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
