9a5fdfde2e122d19db1a13eed9c945efa0a3e285
William Dale <wd40bug@gmail.com>
2026-09-01 17:37:36 -0500

    generation 208

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -19,7 +19,7 @@
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
 
-    programs.gnome-shell.extensions = [
+    services.gnome.shell.extensions = [
       pkgs.gnomeExtensions.accent-icons-theme
     ];
   };
