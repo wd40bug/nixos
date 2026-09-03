1a819904231c6083271af4b8dd8ce8a1b1f1f6fc
William Dale <wd40bug@gmail.com>
2026-09-03 09:30:17 -0500

    generation 263

--- home/wd40bug/pwa/default.nix
+++ home/wd40bug/pwa/default.nix
@@ -37,7 +37,7 @@
             manifestUrl = "https://chat.google.com/u/0/manifest.json";
             desktopEntry.icon = pkgs.fetchurl {
               url = "https://ssl.gstatic.com/dynamite/images/favicons_20260602/chat_2026_logo_favicon_no_dot_256px.png";
-              hash = "sha256-DgLjgCYkkIAT3P5WIaCR6SrNd30asp4PDeRQclsPsEo=";
+              hash = "sha256-r5H4R6QFadGiYrC1Iw/O++J+MpGOU5XRKBVBvGwsbvw=";
             };
           };
         };
