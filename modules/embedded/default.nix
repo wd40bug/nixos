143066b93dc60a3a8eb1c161c27e4a5422b213e0
William Dale <wd40bug@gmail.com>
2026-09-21 16:43:54 -0500

    generation 282

--- modules/embedded/default.nix
+++ modules/embedded/default.nix
@@ -1,10 +1,14 @@
-{lib, config, ...}: {
+{lib, config, pkgs, ...}: {
   options.custom.embedded = {
     enable = lib.mkEnableOption "Enable Embedded Module";
     tangnano.enable = lib.mkEnableOption "Enable Tang Nano 20k";
   };
 
-  config = lib.mkIf config.custom.embedded.enable {
+  config = lib.mkIf config.custom.embedded.enable (with config.custom.embedded;{
     users.groups.dialout.members = ["wd40bug"];
-  };
+
+    environment.systemPackages = lib.mkIftangnano.enable [pkgs.openfpgaloader];
+
+    hardware.openfpgaloader.enable = tangnano.enable;
+  });
 }
