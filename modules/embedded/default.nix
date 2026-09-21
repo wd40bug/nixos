56e02f0cf0813ee5da882bb0b7278b4570770aec
William Dale <wd40bug@gmail.com>
2026-09-21 16:58:58 -0500

    generation 285

--- modules/embedded/default.nix
+++ modules/embedded/default.nix
@@ -19,7 +19,18 @@
         pkgs.openfpgaloader
       ];
 
-      users.groups.plugdev.members = lib.mkIf tangnano.enable ["wd40bug"];
+      users.groups.plugdev.members = lib.mkIf tangnano.enable [ "wd40bug" ];
+
+      users.groups.tty.members = lib.mkIf tangnano.enable [ "wd40bug" ];
+
+      services.udev.extraRules = lib.mkIf tangnano.enable ''
+        # Sipeed / Bouffalo Lab BL702 (Tang Nano 20k)
+        SUBSYSTEM=="usb", ATTR{idVendor}=="303a", ATTR{idProduct}=="1001", MODE="0666", GROUP="dialout"
+        SUBSYSTEM=="usb", ATTR{idVendor}=="303a", ATTR{idProduct}=="1002", MODE="0666", GROUP="dialout"
+
+        # Standard FTDI fallback
+        SUBSYSTEM=="usb", ATTR{idVendor}=="0403", ATTR{idProduct}=="6010", MODE="0666", GROUP="dialout"
+      '';
     }
   );
 }
