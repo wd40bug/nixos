4c88cd2344cdc705d8fad3c0420375778e639847
William Dale <wd40bug@gmail.com>
2026-09-23 18:56:42 -0500

    clangd upgrade

--- home/wd40bug/neovim/nvim/after/lsp/clangd.lua
+++ home/wd40bug/neovim/nvim/after/lsp/clangd.lua
@@ -14,6 +14,7 @@ return {
     'clangd',
     '--query-driver=~/Downloads/arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc,/home/wd40bug/.platformio/packages/toolchain-xtensa-esp32*/bin/xtensa-esp32-elf-g++',
     '--query-driver=/home/wdale/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-g++',
-    '--query-driver=/home/wdale/.platformio/packages/toolchain-intelarc32/bin/arc-elf32-g++'
+    '--query-driver=/home/wdale/.platformio/packages/toolchain-intelarc32/bin/arc-elf32-g++',
+    "--query-driver=**/*gcc*",
   }
 }
